635a737aad079d2110b9909a3694d6955d82a555
MohithSamayam <mohithsamayam802@gmail.com>
2026-03-24 09:05:49 +0530

    Initial commit

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import Test_paragraph from "./test1";
+function TestComponent(){
+    return(
+        <div>
+            <h1>Test Component</h1>
+            <Test_paragraph/>
+        </div>
+    )
+}
+export default TestComponent;
---
+++ src/test1.js
@@ -0,0 +1,7 @@
+import './test1.css'
+function Test_paragraph(){
+    return(
+        <p className='p'>Component in use</p>
+    )
+}
+export default Test_paragraph;
